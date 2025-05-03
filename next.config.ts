9fd9d472bb81e5dc6e22f0b26c7d457261d47528
yuler <is.yuler@gmail.com>
2025-05-03 20:30:03 +0800

    Ignore eslint build error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // refs: https://nextjs.org/docs/app/api-reference/config/eslint#disabling-linting-during-production-builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // refs: https://nextjs.org/docs/app/api-reference/config/next-config-js/typescript
   typescript: {
     ignoreBuildErrors: true,
